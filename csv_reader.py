c0d6115124bcdadd41f615763e428cd7e34b5db5
MichaelKora <michael.koras0@gmail.com>
2023-08-21 10:27:43 +0200

    add py test

--- csv_reader.py
+++ csv_reader.py
@@ -1,11 +1,19 @@
-import pandas as pd
+import csv, sys
   
 # read CSV file
-donations_results = pd.read_csv('Donnations.csv')
-donors_results = pd.read_csv('Donors.csv')
+with open('Donnations.csv', newline='') as f1:
+    donnations_reader = csv.reader(f1)
+    donations_len = sum(1 for row in donnations_reader)  # fileObject is your csv.reader
+    
+with open('Donors.csv', newline='') as f2:
+    donors_reader = csv.reader(f2)
+    donors_len = sum(1 for row in donors_reader)  # fileObject is your csv.reader
 
-donations_len = en(donations_len)
-donors_len = len(donors_results)
+# donations_results = pd.read_csv('')
+# donors_results = pd.read_csv('Donors.csv')
+
+# donations_len = en(donations_len)
+# donors_len = len(donors_results)
 
 print (f"Amount of entries in donations.csv is: {donations_len}")
 print (f"Amount of entries in donors.csv is: {donors_len}")
